b65986b07b5c12e468399a6d4535f4e033c6db80
Sylvain Guilbaud <Sylvain.Guilbaud@intersystems.com>
2021-11-29 18:32:37 +0100

    shell

--- src/shell/transaction.sh
+++ src/shell/transaction.sh
@@ -1,6 +1,23 @@
 # New transaction
+export amount=$1
+echo $amount
+
+generate_post_data()
+{
+  cat <<EOF
+{
+  "TransType": "PAYMENT",
+  "Amount": "${amount}",
+  "FromAccountNumber": "C1315400589",
+  "ToAccountNumber": "M1353266412"
+}
+EOF
+}
+
 curl -i -X POST \
 --url http://localhost:9092/csp/appint/rest/transaction/ \
 -u _system:sys \
 -H 'Content-Type: application/json' \
--d '{"TransType": "PAYMENT","Amount": "7.5","FromAccountNumber": "C1315400589","ToAccountNumber": "M1353266412"}'
+--data "$(generate_post_data)"
+
+# -d '{"TransType": "PAYMENT","Amount": "7.5","FromAccountNumber": "C1315400589","ToAccountNumber": "M1353266412"}' \
